d81befe44259da4ce17055a4b4ce89f59ca6259a
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-12-09 12:21:13 +0100

    event.target: El elemento donde se hizo click

--- 2-javascript/script.js
+++ 2-javascript/script.js
@@ -1,9 +1,3 @@
-// ✅ Un solo evento en el padre
-const container = document.querySelector('#jobs-list')
-
 container.addEventListener('click', function (event) {
-  // Detectamos si el click fue en un botón
-  if (event.target.classList.contains('button-apply-job')) {
-    // Código aquí
-  }
+    console.log('Hiciste click en:', event.target)
 })
