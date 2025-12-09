283273d93c0a6e092648cac111d9a63e679f9706
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-12-09 12:08:43 +0100

    ¿Qué es un NodeList?

--- 2-javascript/script.js
+++ 2-javascript/script.js
@@ -1,5 +1,14 @@
-// Selecciona TODOS los botones con esa clase
 const botones = document.querySelectorAll('.button-apply-job')
 
-console.log(botones) // NodeList(3) [button, button, button]
+// Tiene .length como los arrays
 console.log(botones.length) // 3
+
+// Podemos acceder por índice
+console.log(botones[0]) // Primer botón
+console.log(botones[1]) // Segundo botón
+console.log(botones[2]) // Tercer botón
+
+// Tiene forEach (pero no map, filter, reduce, etc.)
+botones.forEach(function (boton) {
+  console.log(boton)
+})
