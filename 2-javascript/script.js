4e774adbda94578852cd3eb182d195ce4bc5d716
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-12-09 12:03:34 +0100

    Los callbacks se ejecutan cuando ocurre el evento

--- 2-javascript/script.js
+++ 2-javascript/script.js
@@ -1,5 +1,9 @@
+console.log('1. Inicio del script')
+
 const boton = document.querySelector('#boton-importante')
 
 boton.addEventListener('click', function () {
-  alert('¡Hiciste clic!')
+  console.log('3. ¡Click en el botón!')
 })
+
+console.log('2. Fin del script')
