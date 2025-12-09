ac0f3e2a98562f98b14e2c474d064e37b856967e
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-12-09 12:06:18 +0100

    Cambiar el contenido de texto con textContent

--- 2-javascript/script.js
+++ 2-javascript/script.js
@@ -1,16 +1,6 @@
 const boton = document.querySelector('#boton-importante')
 
-const rect = boton.getBoundingClientRect()
-
-console.log(rect)
-
-/* Salida: {
-  width: 120,
-  height: 40,
-  x: 100,
-  y: 200,
-  top: 200,
-  left: 100,
-  right: 220,
-  bottom: 240
-} */
+boton.addEventListener('click', function () {
+  // Cambiar el texto del botón
+  boton.textContent = '¡Aplicado!'
+})
