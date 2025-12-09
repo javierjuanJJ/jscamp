1ccb121d6e42c754edc6eb1546d3bc4a606078f8
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-12-09 12:04:44 +0100

    Propiedades y métodos de los elementos

--- 2-javascript/script.js
+++ 2-javascript/script.js
@@ -1,10 +1,4 @@
-console.log('Inicio: Cargando el script')
-
 const boton = document.querySelector('#boton-importante')
-console.log('Botón encontrado:', boton)
 
-boton.addEventListener('click', function () {
-  console.log('¡Hiciste click!')
-})
-
-console.log('Fin: Script cargado, esperando eventos')
+// ¿Qué es boton?
+console.log(boton) // <button id="boton-importante">Haz clic</button>
