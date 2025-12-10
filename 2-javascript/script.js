dc3846b0a9e124a830b74d5b6d5aa27ebf3b137d
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-12-10 14:17:39 +0100

    input javascript

--- 2-javascript/script.js
+++ 2-javascript/script.js
@@ -10,3 +10,37 @@ jobsListingSection.addEventListener('click', function (event) {
     }
 })
 
+
+
+const inputNombre = document.getElementById('input-nombre')
+inputNombre.addEventListener('input', function () {
+    console.log('Escribiendo... ' , inputNombre.value)
+})
+
+inputNombre.addEventListener('blur', function () {
+    console.log('Perdió el foco')
+})
+
+inputNombre.addEventListener('keydown', function (event) {
+    console.log('La tecla presionada ' , event.key)
+    console.log('El código de la tecla ' , event.code)
+    console.log('Si la tecla se está presionando con la tecla Shift ' , event.shiftKey)
+    console.log('Si la tecla se está presionando con la tecla Ctrl ' , event.ctrlKey)
+    console.log('Si la tecla se está presionando con la tecla Alt ' , event.altKey)
+    console.log('Si la tecla se está presionando con la tecla Meta ' , event.metaKey)
+})
+
+
+inputNombre.addEventListener('keyup', function (event) {
+    console.log('La tecla presionada ' , event.key)
+    console.log('El código de la tecla ' , event.code)
+    console.log('Si la tecla se está presionando con la tecla Shift ' , event.shiftKey)
+    console.log('Si la tecla se está presionando con la tecla Ctrl ' , event.ctrlKey)
+    console.log('Si la tecla se está presionando con la tecla Alt ' , event.altKey)
+    console.log('Si la tecla se está presionando con la tecla Meta ' , event.metaKey)
+})
+
+formulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log('Formulario enviado')
+})
