2b8de301104be6dae1ea86b8ce63936239db7bfd
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-12-15 16:20:35 +0100

    fetch 3

---
+++ 2-javascript/fetch-date.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('.jobs-listings')
+
+fetch('./data.json') // obtenemos el archivo data.json
+  .then((response) => {
+    // convertimos la respuesta a JSON
+    return response.json()
+  })
+  .then((jobs) => {
+    // recorremos los datos y los mostramos en el DOM
+    jobs.forEach((job) => {
+      // creamos un elemento HTML <article> para cada trabajo
+      const article = document.createElement('article')
+      // añadimos la clase job-listing-card al elemento
+      article.className = 'job-listing-card'
+      // añadimos los datos al elemento
+      article.dataset.modalidad = job.data.modalidad
+      article.dataset.nivel = job.data.nivel
+      article.dataset.technology = job.data.technology
+
+      // añadimos el contenido HTML al elemento
+      article.innerHTML = `<div>
+          <h3>${job.titulo}</h3>
+          <small>${job.empresa} | ${job.ubicacion}</small>
+          <p>${job.descripcion}</p>
+        </div>
+        <button class="button-apply-job">Aplicar</button>`
+
+      // añadimos el elemento al contenedor
+      container.appendChild(article)
+    })
+  })
