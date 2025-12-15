e207b83794f0ea20a60b22d5a15cc02865bfe96c
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-12-15 16:15:40 +0100

    fetch 1

--- 2-javascript/script.js
+++ 2-javascript/script.js
@@ -14,7 +14,7 @@ jobsListingSection.addEventListener('click', function (event) {
 
 const inputNombre = document.getElementById('input-nombre')
 inputNombre.addEventListener('input', function () {
-    console.log('Escribiendo... ' , inputNombre.value)
+    console.log('Escribiendo... ', inputNombre.value)
 })
 
 inputNombre.addEventListener('blur', function () {
@@ -22,25 +22,42 @@ inputNombre.addEventListener('blur', function () {
 })
 
 inputNombre.addEventListener('keydown', function (event) {
-    console.log('La tecla presionada ' , event.key)
-    console.log('El código de la tecla ' , event.code)
-    console.log('Si la tecla se está presionando con la tecla Shift ' , event.shiftKey)
-    console.log('Si la tecla se está presionando con la tecla Ctrl ' , event.ctrlKey)
-    console.log('Si la tecla se está presionando con la tecla Alt ' , event.altKey)
-    console.log('Si la tecla se está presionando con la tecla Meta ' , event.metaKey)
+    console.log('La tecla presionada ', event.key)
+    console.log('El código de la tecla ', event.code)
+    console.log('Si la tecla se está presionando con la tecla Shift ', event.shiftKey)
+    console.log('Si la tecla se está presionando con la tecla Ctrl ', event.ctrlKey)
+    console.log('Si la tecla se está presionando con la tecla Alt ', event.altKey)
+    console.log('Si la tecla se está presionando con la tecla Meta ', event.metaKey)
 })
 
 
 inputNombre.addEventListener('keyup', function (event) {
-    console.log('La tecla presionada ' , event.key)
-    console.log('El código de la tecla ' , event.code)
-    console.log('Si la tecla se está presionando con la tecla Shift ' , event.shiftKey)
-    console.log('Si la tecla se está presionando con la tecla Ctrl ' , event.ctrlKey)
-    console.log('Si la tecla se está presionando con la tecla Alt ' , event.altKey)
-    console.log('Si la tecla se está presionando con la tecla Meta ' , event.metaKey)
+    console.log('La tecla presionada ', event.key)
+    console.log('El código de la tecla ', event.code)
+    console.log('Si la tecla se está presionando con la tecla Shift ', event.shiftKey)
+    console.log('Si la tecla se está presionando con la tecla Ctrl ', event.ctrlKey)
+    console.log('Si la tecla se está presionando con la tecla Alt ', event.altKey)
+    console.log('Si la tecla se está presionando con la tecla Meta ', event.metaKey)
 })
 
 formulario.addEventListener('submit', function (event) {
     event.preventDefault();
     console.log('Formulario enviado')
+})
+
+
+fetch('https://jsonplaceholder.typicode.com/todos').then((response) => {
+    console.log(response.ok) // true si la petición salió bien
+    console.log(response.status) // código HTTP, por ejemplo 200
+})
+
+
+fetch('./data.json')
+.then((response) => {
+    return response.json();
+}).then((data) => {
+    console.log(data)
+})
+.catch((error) => {
+    console.error('No pudimos obtener los datos', error)
 })
